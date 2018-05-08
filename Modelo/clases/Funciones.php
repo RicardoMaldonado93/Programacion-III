f210ab55c411e7db67da0fcf8eb4efaad3648418
RicardoMaldonado93 <maldonadoricardo93@gmail.com>
2018-05-08 18:01:42 -0300

    Add files via upload

--- Modelo/clases/Funciones.php
+++ Modelo/clases/Funciones.php
@@ -44,7 +44,7 @@
      function cargarHelado($sabor, $precio, $foto)
     {
         $helado = new Helado($sabor, $precio, $foto);
-        $archivo = fopen("./helados/sabores.txt", "a+");
+        $archivo = fopen("./helados/sabores.txt", "a");
         if(fwrite($archivo, $helado->__toString()) != false)
             if(move_uploaded_file($foto['imagen']['tmp_name'], "./helados/heladosImagen/". $helado->Archivo($foto)))
                 echo "SE AGREGO CORRECTAMENTE EL SABOR<br>";
@@ -105,7 +105,7 @@
     {
         $helados = Helado::Lista();
         $ruta;
-   echo "    <div align= 'center'> <table border='2' bordercolor ='green'>
+            echo "  <div align= 'center'> <table border='2' bordercolor ='green'>
                     <th colspan='3'>HELADOS</th>
                     <tr>
                     <th>SABOR</th>
@@ -118,8 +118,8 @@
             {
                 
                 $ruta = "./helados/heladosImagen/". $sabor[2];
-                echo "<tr><td align = 'middle' >".$sabor[0]."</td>
-                      <td align = 'middle' >".$sabor[1]."</td>
+                echo "<tr><td align = 'middle'>".strtoupper($sabor[0])."</td>
+                      <td align = 'middle' >".strtoupper($sabor[1])."</td>
                       <td>";
                       echo "<img src='" . $ruta . "' width = '80px' heigth = '80px'>";
                 echo"</td></tr></div>";
@@ -131,7 +131,7 @@
      function ObtenerHelados()
     {
         $archivo = fopen("./helados/sabores.txt", "r");
-        $helados = array();
+        $helados = array(); 
         
         while(!feof($archivo))
             $helados[] = explode("*",fgets($archivo));
@@ -141,48 +141,120 @@
         return $helados;
     }
 
-     function borrarHelados($caso)
+     function borrarHelados($caso , $param)
     {
-        $archivo = fopen("./helados/sabores.txt", "a+"); 
-        $helado = ObtenerHelados();
-        $band = false ; $saborObtenido;
+        $archivo = fopen("./helados/sabores.txt", "r"); 
+        $helado = ObtenerHelados(); $band = false;
         
-
-        if($caso != "borrarHelado" && $caso != NULL)
+        if($param == 1)
+        {
+            if($caso != "borrarHelado" && $caso != NULL)
             foreach($helado as $index => $valor)
             {
                 if(isset($valor[0]) && $valor[0] == $caso)
                     {
-                        $saborObtenido = $caso;
-                        $band = true;
+                        echo "SE HA ENCONTRADO EL SABOR " . strtoupper($caso) . "<br>";
+                        return $caso;
                     }
 
             }
-        var_dump($caso);
-        if($band == true && $caso != NULL)
-        {
-            if($caso == "borrarHelado")
-                {
-
-                    foreach($helados as $index => $valor)
-                        if(isset($valor) && $valor != $saborObtenido)
-                        {
-                            $N_helados[] = $valor;
-                            $name = explode(".", $valor[2]);
-                            $rename = $name[0] . "." . "borrado" . date("Hsi") . "." . $name[2];
-                            if(move_uploaded_file("./helados/heladosImagen/".$valor[2], "./heladosBorrados" . $rename))
-                                echo "SE HA LOGRADO BORRAR EL SABOR " . strtoupper($name[0]) . "<br>";
-                        }
-
-                    foreach($N_helados as $index=> $valor)
-                        fwrite($archivo, $valor[0].'*'.$valor[1].'*'.$valor[2]. PHP_EOL);
-                    
-                    fclose($archivo);
-                }
+            else
+            {
+                echo "NO SE HA ENCONTRADO EL SABOR **" . strtoupper($caso)."**";
+            }
         }
+            
+        elseif($caso == "borrarHelado")
+            {
+                
+                $archivo = fopen("./helados/sabores.txt", "w+"); 
 
-        if($band == false)
-                echo "NO EXISTE EL SABOR<br>";
+                foreach($helado as $index => $valor)
+                    if(isset($valor[0]) && $valor[0] != $param)
+                        $N_helados[] = $valor;
+                
+                    foreach($helado as $index => $valor)
+                        if( $valor[0] == $param)
+                            {
+                                echo "estoy moviendo el archivo";
+                                $name = explode(".", $valor[2]);
+                                $rename = $name[0] . "." . "borrado" . "." . date("Hsi") . "." . rtrim($name[2],PHP_EOL);
+                                $name = $name[0] . '.' . $name[1] .".". rtrim($name[2], PHP_EOL);
+                                $ruta = "./helados/heladosImagen/".$name;
+                                $destino ="./clases/heladosBorrados/".$rename;
+
+                                if(copy($ruta,$destino))
+                                    unlink($ruta);
+                                $band = true;
+                            }
+                
+                foreach($N_helados as $index=> $valor)
+                    if( $valor[0] != "")
+                        {
+                            echo"<br>";
+                            $str = $valor[0].'*'.$valor[1].'*'. $valor[2];
+                            fwrite($archivo, $str);
+                        }
+                fclose($archivo);
+
+                if($band == true)
+                    return true;
+            
+                else
+                    echo "NO SE ENCUENTRA EL SABOR **" . strtoupper($param) ."**<br>";
+            }
+
+    if($band = false)
+        echo "NO SE ENCUENTRA EL SABOR **" . strtoupper($param) ."**<br>";
+        
     }
 
+    function ModificarHelado($sabor, $precio, $foto)
+    {
+        $archivo = fopen("./helados/sabores.txt", "r"); 
+        $helado = ObtenerHelados();
+
+        foreach($helado as $index => $valor)
+            if(isset($valor[0]) && $valor[0] == $sabor)
+            {
+                if(borrarHelados("borrarHelado", $sabor));
+                {
+                    cargarHelado($sabor,$precio,$foto);
+                    return true;
+                }
+            }
+
+    } 
+
+    function listar_directorios_ruta($ruta){ 
+        // abrir un directorio y listarlo recursivo 
+        if (is_dir($ruta)) { 
+           if ($dh = opendir($ruta)) { 
+              while (($file = readdir($dh)) !== false) { 
+                 //esta línea la utilizaríamos si queremos listar todo lo que hay en el directorio 
+                 //mostraría tanto archivos como directorios 
+                 //echo "<br>Nombre de archivo: $file : Es un: " . filetype($ruta . $file); 
+                 if (is_dir($ruta . $file) && $file!="." && $file!=".."){ 
+                    //solo si el archivo es un directorio, distinto que "." y ".." 
+                    echo "<br>Directorio: $ruta$file"; 
+                    listar_directorios_ruta($ruta . $file . "/"); 
+                 } 
+              } 
+           closedir($dh); 
+           } 
+        }else 
+           echo "<br>No es ruta valida"; 
+     }
+     function listar_archivos($carpeta){
+        if(is_dir($carpeta)){
+            if($dir = opendir($carpeta)){
+                while(($archivo = readdir($dir)) !== false){
+                    if($archivo != '.' && $archivo != '..' && $archivo != '.htaccess'){
+                        echo '<li><a target="_blank" href="'.$carpeta.'/'.$archivo.'">'.$archivo.'</a></li>';
+                    }
+                }
+                closedir($dir);
+            }
+        }
+    }
 ?>
--- Modelo/nexo.php
+++ Modelo/nexo.php
@@ -1,60 +1,71 @@
 <?php
-        require_once("./clases/Funciones.php");
-        $ope; $band = 0;
+    
+    require_once("./clases/Funciones.php");
 
-        if(isset($_GET))
-        {
-            $ope = $_GET['op'];
-            $band = 1;
-        }
-        else
-            $ope = $_POST['op'];
-
-
-    switch($ope)
+    if(isset($_POST['op']))
     {
-        case 'cargarCliente':
-            {
-                cargarCliente($_POST['nombre'],$_POST['correo'],$_POST['pass']);
-                break;
-            }
+        switch($_POST['op'])
+        {
+            case 'cargarCliente':
+                {
+                    cargarCliente($_POST['nombre'],$_POST['correo'],$_POST['pass']);
+                    break;
+                }
 
-        case 'validar':
-            {
-                echo validar($_POST['correo'],$_POST['pass']);
-                break;
-            }
+            case 'validar':
+                {
+                    echo validar($_POST['correo'],$_POST['pass']);
+                    break;
+                }
 
-        case 'cargarHelado':
-            {
-                cargarHelado($_POST['sabor'],$_POST['precio'],$_FILES);
-                break;
-            }
-        
-        case 'vender':
-            {
-                echo vender($_GET['sabor'],$_GET['cant']);
-                break;
-            }
+            case 'cargarHelado':
+                {
+                    cargarHelado($_POST['sabor'],$_POST['precio'],$_FILES);
+                    break;
+                }
 
-        case 'listadoHelados':
-            {
-                ListadoHelados();
-                break;
-            }
-
-        case 'borrarHelado':
-            {
-                if($band == 1)
-                    borrarHelados($_GET['sabor']);
-                else
-                    borrarHelados($_POST['caso']);
-                break;
-            }
-        default:
-            {
-                echo "ERROR!!! VERIFIQUE INSTRUCCION";
-                break;
-            }
+            case 'borrarHelado':
+                {
+                    $sabor = borrarHelados($_POST['sabor'], 1);
+                    setcookie('helado',$sabor);
+                    break;
+                }
+            case 'modificarHelado':
+                    {
+                        if(modificarHelado($_POST['sabor'],$_POST['precio'],$_FILES))
+                            echo "SE HA MODIFICADO EL HELADO DE " . $_POST['sabor'];
+                    }
+            default:
+                {
+                    echo "ERROR!!! VERIFIQUE INSTRUCCION";
+                    break;
+                }
+        }
     }
+    elseif(isset($_GET['op']))
+    {
+        switch($_GET['op'])
+        {
+            case 'vender':
+                {
+                    echo vender($_GET['sabor'],$_GET['cant']);
+                    break;
+                }
+
+            case 'listadoHelados':
+                {
+                    ListadoHelados();
+                    break;
+                }
+            
+            case 'borrarHelado':
+                {
+                    if(borrarHelados($_GET['op'], $_COOKIE['helado']))
+                    echo "SE HA BORRADO CON EXITO EL SABOR **" . strtoupper($_COOKIE['helado']) ."**<br>";
+                    break;
+                }
+        }
+    }
+
+
 ?>
